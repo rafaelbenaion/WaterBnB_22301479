8cdc9e6400601814744f9b6674401c3aa6d85db6
Rafael Baptista <baprafael@icloud.com>
2024-01-16 01:16:25 +0100

    Setting up /open path

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+# -------------------------------------------------------------------------------------------------------- #
+# IOT, WaterBnB Project - Flask server                                                                     #
+# -------------------------------------------------------------------------------------------------------- #
+# 16 Jan 2024, Université Côte d'Azur,                                                                     #
+# CRafael Baptista.                                                                                        #
+# -------------------------------------------------------------------------------------------------------- #
+
+from urllib import request
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+# Print all get parameters from the request /open?idu=Rafael&idswp=P_22301479
+@app.route('/open')
+def open():
+    idu = request.args.get('idu', 'No idu provided')
+    idswp = request.args.get('idswp', 'No idswp provided')
+    return f'idu: {idu}, idswp: {idswp}'
